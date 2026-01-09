b03faac1319a35fd37524ce0ac0fe667a830c521
Kanishkha A R <kanishkha57@gmail.com>
2026-01-09 16:08:18 +0530

    Time: 1 ms (99.90%) | Memory: 43.3 MB (44.13%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];  // ASCII size
+        for (char c : s.toCharArray()) {
+            count[c]++;
+        }
+        int length = 0;
+        boolean oddFound = false;
+        for (int c : count) {
+            if (c % 2 == 0) {
+                length += c;
+            } else {
+                length += c - 1;
+                oddFound = true;
+            }
+        }
+        if (oddFound) length++;
+        return length;
+    }
+}
+    
